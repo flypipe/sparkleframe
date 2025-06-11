4fcb6d45d3218033453d2e130ec06a7a02790172
josemuller <joseheliomuller@gmail.com>
2025-06-11 15:56:13 -0300

    #33-fix-import-error-of-functions add sparkleframe.polarsdf.functions import

--- sparkleframe/polarsdf/__init__.py
+++ sparkleframe/polarsdf/__init__.py
@@ -4,5 +4,6 @@ from sparkleframe.polarsdf.dataframe import DataFrame
 from sparkleframe.polarsdf.session import SparkSession
 from sparkleframe.polarsdf.types import StringType
 from sparkleframe.polarsdf.window import Window, WindowSpec
+import sparkleframe.polarsdf.functions
 
 __all__ = ["Column", "SparkSession", "DataFrame", "Window", "WindowSpec"]
